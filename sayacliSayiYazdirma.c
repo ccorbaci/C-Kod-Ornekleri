474be983f59e225ca18f9d88ea63e5046e21932d
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2022-12-07 00:54:36 +0300

    Create sayacliSayiYazdirma.c

---
+++ sayacliSayiYazdirma.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//10 adet sayıyı sayac kullanarak yazdırın//
+
+int main() {
+	
+	int sayac;
+	int i;
+	for(i<=0;i<=10;i++)
+	{
+		sayac=sayac+i;
+		printf("%d\n",sayac);
+	}
+	return 0;
+}
