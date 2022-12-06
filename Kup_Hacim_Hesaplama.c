0046e88fe2484fb6347cb6e67ce24e5308843c71
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2022-12-07 00:41:10 +0300

    Create Kup_Hacim_Hesaplama.c

---
+++ Kup_Hacim_Hesaplama.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+int kenar;
+			printf("**Kup Hacim hesaplaticiya hos geldiniz**\n\n");
+			printf("**Lutfen Kupun Bir Kenarini Giriniz**\n\n");
+			scanf("%d",&kenar);
+			kenar=kenar*kenar*kenar;
+			printf("**Girilen Kupün Alani %d dir",kenar);
+	return 0;
+}
