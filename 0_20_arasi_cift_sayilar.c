233b42cd83c97e4e62ef452622f1e57ee0e0b731
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2022-12-07 00:45:20 +0300

    Create 0_20_arasi_cift_sayilar.c

---
+++ 0_20_arasi_cift_sayilar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//0 dan 20 ye kadar olan çift sayıları yazdırın//
+
+int main() {
+	
+	int i;
+	for(i=0;i<=20;i+=2)
+	{
+		printf("%d-Cift Sayidir\n\n",i);
+	}
+	return 0;
+}
