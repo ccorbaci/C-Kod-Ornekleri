ece380e428b6ae390726377a000af5595c6afd36
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2022-12-07 00:40:09 +0300

    Create karenin_alani.c

---
+++ karenin_alani.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+printf("**Karede alan ve Cevre Hesaplatiya Hosgeldiniz**\n\n");
+				int kenar,alan,cevre;
+				printf("Lutfen Karenin Bir Kenarini Girin : ");
+				scanf("%d",&kenar);
+				alan=kenar*4;
+				cevre=kenar*kenar;
+				printf("Karenin Alani %d dir\n",alan);
+				printf("Karenin Cevresi %d dir\n",cevre);
+	return 0;
+}
