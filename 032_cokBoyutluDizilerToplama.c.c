ff96e343a7558a1bb8309142938dbcb52ee26cb1
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2022-12-09 03:20:29 +0300

    Create 032_cokBoyutluDizilerToplama.c.c

---
+++ 032_cokBoyutluDizilerToplama.c.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//a ve b ikili matrisi toplayan programı yazınız//
+
+int main() {
+	int m1[2][2]={5,10,15,20};
+	int m2[2][2]={30,40,50,60};
+	int toplam[2][2];
+	int i,j,k,l;
+	
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			toplam[i][j]=m1[i][j]+m2[i][j];
+		}
+	}
+	for(k=0;k<2;k++)
+	{
+		for(l=0;l<2;l++)
+		{
+			printf("%d ",toplam[k][l]);
+		}
+		printf("\n");
+	}
+	
+return 0;
+}
