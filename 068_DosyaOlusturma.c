42b4c4036dffa7348673e525cf38ec626e09fdee
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2022-12-13 05:27:43 +0300

    Create 068_DosyaOlusturma.c

---
+++ 068_DosyaOlusturma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//C:\\Users\\ccorb\\Desktop\\asd\\Yeni klasör (6)\\test.txt dizininde yeni dosya oluşturma//
+
+int main() {
+	
+	FILE *dosya;
+	dosya=fopen("C:\\Users\\ccorb\\Desktop\\asd\\Yeni klasör (6)\\test.txt","w");
+	
+	return 0;
+}
