12100e13ef3c1478a0cdcbc47916a71c281135c5
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2022-12-13 04:13:07 +0300

    Create 064_fonksiyonOrnek3.c

---
+++ 064_fonksiyonOrnek3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Klavyeden girilen sayinin küpünü bulan fonksiyon içeren programı kodlayiniz.//
+
+int kupbul(int sayi)
+{
+	int sonuc=sayi*sayi*sayi;
+	return sonuc;
+}
+
+int main() {
+	
+	int s;
+	printf("Sayiyi Giriniz: ");
+	scanf("%d",&s);
+	
+	kupbul(s);
+	
+	printf("Sonuc : %d",kupbul(s));
+
+	return 0;
+}
