cc85e199cbc9e659221281614932e55d9ab6dee4
Cüneyt Çorbacı <53232888+ccorbaci@users.noreply.github.com>
2022-12-16 22:33:17 +0300

    Create 070_carpimTablosu.c

---
+++ 070_carpimTablosu.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+int i,j;
+
+printf("****Carpim Tablosu****\n\n");
+
+	for(i=1;i<=10;i++)
+	{
+		for(j=1;j<=10;j++)
+			{
+				printf(" %d ",i*j);
+			}
+			printf("\n");
+}
+
+return 0;
+}
